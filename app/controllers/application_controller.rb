84f9cc461ac730f1620fbaa7ccc7d078ad4365d0
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-30 16:44:13 +0000

    convention over configuration in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,22 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :private, :name, :bio, :website, :avatar_image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :private, :name, :bio, :website, :avatar_image])
   end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      
+      redirect_back fallback_location: root_url
+    end
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,3 @@
+def feed?
+  true
+end
