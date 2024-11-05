b14d8fa43560321f8e77232e014668ec92e94345
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-11-05 22:06:37 +0000

    added follow_request policy

--- app/policies/follow_request_policy.rb
+++ app/policies/follow_request_policy.rb
@@ -10,7 +10,12 @@ class FollowRequestPolicy < ApplicationPolicy
     true
   end
 
-  def show?
-    true
+  def destroy?
+    user.id == follow_request.sender_id ||
+    user.id == follow_request.recipient_id
+  end
+
+  def update?
+    user.id == follow_request.recipient_id
   end
 end
